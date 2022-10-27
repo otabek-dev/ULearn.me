939201786d8c1f92e5c343dfd1fc49479059eba3
mentos099 <otabek.pro@hotmail.com>
2022-10-27 19:58:13 +0300

    Расстояние от точки (x, y) до отрезка AB с координатами A(ax, ay), B(bx, by)

--- distance/DistanceTask.cs
+++ distance/DistanceTask.cs
@@ -7,12 +7,17 @@ namespace DistanceTask
 		// Расстояние от точки (x, y) до отрезка AB с координатами A(ax, ay), B(bx, by)
 		public static double GetDistanceToSegment(double ax, double ay, double bx, double by, double x, double y)
 		{
+            if (ax == ay && ay == bx && bx == by && by == x && x == y)
+                return 0;
+            if (ax == bx && ay == by)
+                return Math.Sqrt(Math.Pow((x - ax), 2) + Math.Pow((y - ay), 2));
 
 
-			var result = Math.Abs( x*(by-ay) - y*(bx-ax) + bx * ay - by * ax ) / (Math.Sqrt( Math.Pow((by - ay),2) + Math.Pow((bx - ax),2) ));
-
-
-			return result;
-		}
-	}
+            double t = ((x - ax) * (bx - ax) + (y - ay) * (by - ay)) / (Math.Pow((bx - ax), 2) + Math.Pow((by - ay), 2));
+            t = t < 0 ? 0 : t > 1 ? 1 : t;
+            return (double)Math.Sqrt(Math.Pow((ax - x + (bx - ax) * t), 2) + Math.Pow((ay - y + (by - ay) * t), 2));
+            
+            //решение почерпнуто из https://ru.stackoverflow.com/questions/721414
+        }
+    }
 }

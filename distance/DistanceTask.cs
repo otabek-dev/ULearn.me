3bb14066cc8439408ffbb0bc1f3e28a40cb676d0
mentos099 <otabek.pro@hotmail.com>
2022-10-27 20:14:42 +0300

    Added new project TextAnalysis

--- distance/DistanceTask.cs
+++ distance/DistanceTask.cs
@@ -12,7 +12,6 @@ namespace DistanceTask
             if (ax == bx && ay == by)
                 return Math.Sqrt(Math.Pow((x - ax), 2) + Math.Pow((y - ay), 2));
 
-
             double t = ((x - ax) * (bx - ax) + (y - ay) * (by - ay)) / (Math.Pow((bx - ax), 2) + Math.Pow((by - ay), 2));
             t = t < 0 ? 0 : t > 1 ? 1 : t;
             return (double)Math.Sqrt(Math.Pow((ax - x + (bx - ax) * t), 2) + Math.Pow((ay - y + (by - ay) * t), 2));

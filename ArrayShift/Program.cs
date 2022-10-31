496e44868141c82f277cd5ab4e0c095a9fd368c3
mentos099 <otabek.pro@hotmail.com>
2022-10-31 09:55:14 +0300

    Задача на сдвиг элементов массива на k позиций

---
+++ ArrayShift/Program.cs
@@ -0,0 +1,34 @@
+﻿int arrayLength;
+
+while (true)
+{
+	Console.Write("Enter array length: ");
+	var userLength = Console.ReadLine();
+	if (int.TryParse(userLength,out arrayLength))
+		break;
+
+	Console.WriteLine("Error!");
+}
+
+var shiftArray = new int[arrayLength];
+for (int i = 0; i < shiftArray.Length; i++)
+	shiftArray[i] = i + 1;
+
+var array = new int[arrayLength];
+var shift = 2;
+
+for (int i = 0; i < arrayLength; i++)
+{
+	if (i + shift < arrayLength)
+	{
+		array[i] = shiftArray[i + shift];
+	}
+	else
+	{
+		array[i] = shiftArray[shiftArray.Length - i - shift];
+		shift -= 1;
+	}
+}
+
+foreach (var e in array)
+	Console.Write(e + " ");

0c33cb177f8cbf1ae7d0d5eab099e7547c519885
Otabek Rustamov <otabek.pro@hotmail.com>
2023-05-16 02:42:53 +0500

    Done!!

--- PocketGoogle/Indexer.cs
+++ PocketGoogle/Indexer.cs
@@ -1,31 +1,86 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PocketGoogle
 {
     public class Indexer : IIndexer
     {
+        private Dictionary<string, Dictionary<int, List<int>>> documents
+            = new Dictionary<string, Dictionary<int, List<int>>>();
+
         public void Add(int id, string documentText)
         {
-            throw new NotImplementedException();
+            var separators = new char[]
+            {
+                ' ', '.', ',', '!', '?',
+                ':', '-', '\r', '\n', '–'
+            };
+
+            var startIndex = 0;
+            var text = documentText
+                .Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var word in text)
+            {
+                if (documents.ContainsKey(word))
+                    if (documents[word].ContainsKey(id))
+                        documents[word][id]
+                            .Add(documentText
+                            .IndexOf(word, startIndex));
+                    else
+                        documents[word].Add(
+                             id,
+                             new List<int>() 
+                             { 
+                                 documentText.IndexOf(word, startIndex) 
+                             }
+                        );
+                else
+                    documents.Add(word,
+                        new Dictionary<int, List<int>>()
+                        {
+                            { 
+                                id, 
+                                new List<int>() 
+                                { 
+                                    documentText
+                                    .IndexOf(word, startIndex) 
+                                } 
+                            }
+                        });
+
+                startIndex += word.Length;
+            }
         }
 
         public List<int> GetIds(string word)
         {
-            throw new NotImplementedException();
+            var res = new List<int>();
+            if (documents.ContainsKey(word))
+                foreach (var item in documents[word].Keys)
+                    res.Add(item);
+
+            return res;
+
+            //var res2 = documents.Where(d => d.Key == word).Select(d=> d.Value.Keys.ToList());
         }
 
         public List<int> GetPositions(int id, string word)
         {
-            throw new NotImplementedException();
+            if (documents.ContainsKey(word))
+                if (documents[word].ContainsKey(id))
+                    return documents[word][id];
+                else
+                    return new List<int>();
+            else
+                return new List<int>();
         }
 
         public void Remove(int id)
         {
-            throw new NotImplementedException();
+            foreach (var word in documents.Keys)
+                if (documents[word].ContainsKey(id))
+                    documents[word].Remove(id);
         }
     }
 }
